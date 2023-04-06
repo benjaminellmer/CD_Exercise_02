cd7970c95cb0653b67773f2e6664c4c025432c8f
benjaminellmer <benjamin.ellmer@yahoo.com>
2023-04-06 17:35:51 +0200

    Implement model methods

--- model.go
+++ model.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type product struct {
@@ -12,21 +11,50 @@ type product struct {
 }
 
 func (p *product) getProduct(db *sql.DB) error {
-	return errors.New("not implemented")
+	// Scan copies the values of the first matched element to the given target
+	return db.QueryRow("SELECT name,price FROM products WHERE id=$1", p.ID).Scan(&p.Name, &p.Price)
 }
 
 func (p *product) updateProduct(db *sql.DB) error {
-	return errors.New("not implemented")
+	_, err := db.Exec("UPDATE products SET name=$1, price=$2 WHERE id=$3", p.Name, p.Price, p.ID)
+
+	return err
 }
 
 func (p *product) deleteProduct(db *sql.DB) error {
-	return errors.New("not implemented")
+	_, err := db.Exec("DELETE from products WHERE id=$1", p.ID)
+
+	return err
 }
 
 func (p *product) createProduct(db *sql.DB) error {
-	return errors.New("not implemented")
+	err := db.QueryRow("INSERT INTO products(name, price) VALUES($1, $2) RETURNING id", p.Name, p.Price).Scan(&p.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func getProducts(db *sql.DB, start int, count int) ([]product, error) {
-	return nil, errors.New("not implemented")
+	rows, err := db.Query("SELECT id, name, price FROM products LIMIT $1 OFFSET $2", count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Note: This block is then executed when the function returns
+	defer rows.Close()
+
+	var products []product
+
+	for rows.Next() {
+		var p product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, nil
 }
